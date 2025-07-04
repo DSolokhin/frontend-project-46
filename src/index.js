248fdf7b0b6e8850fbdcdc69da6f98c9af729ecb
DS <sda@uniavds.com>
2025-07-04 16:55:24 +0300

    Fix lint

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const data1 = getDataFromFile(filepath1)
   const data2 = getDataFromFile(filepath2)
   const diff = buildDiff(data1, data2)
-  
+
   switch (format) {
     case 'stylish':
       return stylish(diff)
